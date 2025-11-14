c870b26171489d4dea163e351ef1ea38f99ef16a
Michael <mikebgallagher@mac.com>
2025-11-14 10:28:12 -0800

    Fix: Rename Pralaya.webp to lowercase, cleanup old files

--- scripts/renderWelcomeEmail.ts
+++
@@ -1,6 +0,0 @@
-// scripts/renderWelcomeEmail.ts
-import { render } from "@react-email/render";
-import { WelcomeEmail } from "../supabase/emails/WelcomeEmail"; // or wherever it's located
-
-const emailHtml = render(<WelcomeEmail />);
-console.log(emailHtml);
